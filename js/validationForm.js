bc42015496d3b296e5a06e8f06b62573ae85409e
YuraSadilenko <yura.sadilenko.it@gmail.com>
2018-12-30 11:21:57 +0200

    final cut

--- js/validationForm.js
+++ js/validationForm.js
@@ -57,3 +57,80 @@ var form3 = {
 		errorMessage: '',
 	},
 };
+
+var validationForm = validation();
+
+console.log(validationForm(form1));
+console.log(validationForm(form2));
+console.log(validationForm(form3));
+
+function validation() {
+  var results = [];
+  function checkResults(obj) {
+    for(key in obj) {
+      if(obj[key].validationRules.required) {
+        results.push(checkRequired(obj[key]));
+      }
+      if(obj[key].validationRules.minLength) {
+        results.push(checkMinLength(obj[key]));
+      }
+      if(obj[key].validationRules.maxLength) {
+        results.push(checkMaxLength(obj[key]));
+      }
+      if(obj[key].validationRules.email) {
+        results.push(checkEmail(obj[key]));
+      }
+    }
+    return results.every(checkedResults);
+  }
+
+  function checkRequired(obj) {
+    var value = obj.value;
+    if ((value.length === 0) || value.split('').some(checkSpaces)) {
+      obj.errorMessage = 'ERROR: This is required field';
+      return false;
+    }
+    return true;
+  }
+
+  function checkMinLength(obj) {
+    var value = obj.value;
+    var requireMinLength = obj.validationRules.minLength;
+    if (value.length >= requireMinLength) {
+      return true;
+    }
+    obj.errorMessage = 'ERROR: Min length is ' + requireMinLength;
+    return false;
+  }
+
+  function checkMaxLength(obj) {
+    var value = obj.value;
+    var requireMaxLength = obj.validationRules.maxLength;
+    if (value.length <= requireMaxLength) {
+      return true;
+    }
+    obj.errorMessage = 'ERROR: Max length is ' + requireMaxLength;
+    return false;
+  }
+
+  function checkEmail(obj) {
+    var email = obj.value;
+    var ruOrUa = email.slice(-3);
+    var comOrOrg = email.slice(-4);
+    if (email.indexOf('@') !== -1 && (ruOrUa === '.ru' || ruOrUa === '.ua' || comOrOrg === '.com' || comOrOrg === '.org')) {
+      return true;
+    }
+    obj.errorMessage = 'ERROR: Your email is invalid';
+    return false;
+  }
+
+  function checkedResults(item) {
+    return item === true;
+  }
+
+  function checkSpaces(item) {
+    return item === ' ';
+  }
+
+  return checkResults;
+}
